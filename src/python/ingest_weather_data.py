4c3062ba6ad74c19515b3bf26971b1f1150c372d
Tomas Ekholm <86534388+ektomath@users.noreply.github.com>
2026-06-29 15:29:53 +0200

    Removed 2 variables from the data that we request from open-meteo. Created helper functions to make code more readable. Added type hints and small docstrings to the code.

--- src/python/ingest_weather_data.py
+++ src/python/ingest_weather_data.py
@@ -9,6 +9,7 @@ import time
 from datetime import datetime, timezone
 from pathlib import Path
 from retry_requests import retry
+from typing import Any
 
 
 # CONSTANTS
@@ -18,27 +19,74 @@ LOCATIONS_FILE  = PROJECT / "data" / "bronze" / "airport_locations_raw.csv" # TO
 OUTPUT_DIR      = PROJECT / "data" / "silver"
 
 URL             = "https://archive-api.open-meteo.com/v1/archive"
-
 VARIABLES       = [
     "temperature_2m_mean",
-    "temperature_2m_max",
-    "temperature_2m_min",
     "precipitation_sum",
     "wind_speed_10m_max"
 ]
 
 # HELPER FUNCTIONS
 
-def setup_api_client():
-    # Setup the Open-Meteo API client with cache and retry on error
-    cache_session   = requests_cache.CachedSession('.cache', expire_after = 3600)
-    retry_session   = retry(cache_session, retries = 5, backoff_factor = 0.2)
-    openmeteo       = openmeteo_requests.Client(session = retry_session)
-    
+def setup_api_client() -> openmeteo_requests.Client:
+    """Create API client."""
+
+    cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
+
     return openmeteo
 
 
-def fetch_weather_batches(openmeteo, locations, batch_size=10):
+def request_with_retry(openmeteo: openmeteo_requests.Client, params: dict[str, Any]) -> list[Any]:
+    """
+    Request data with retries. 
+    This is necessary because open-meteo limits the amount of data one can fetch per minute and per hour.
+    """
+
+    tries = 0
+
+    while True:
+        try:
+            tries += 1
+            responses = openmeteo.weather_api(URL, params=params)
+            print(f"request succeeded on try {tries}")
+            return responses
+        except Exception as e:
+            print(f"request failed on try {tries}, waiting 60 seconds: {e}")
+            time.sleep(60)
+
+
+def response_to_dataframe(response: Any) -> pd.DataFrame:
+    """Convert data from response objects to DataFrame."""
+
+    daily = response.Daily()
+
+    dates = pd.date_range(
+        start=pd.to_datetime(daily.Time(), unit="s", utc=True),
+        periods=len(daily.Variables(0).ValuesAsNumpy()),
+        freq="D",
+    )
+
+    return pd.DataFrame({
+        "longitude": response.Longitude(),
+        "latitude": response.Latitude(),
+        "year": dates.year,
+        "month": dates.month,
+        "day": dates.day,
+        "temperature_2m_mean": daily.Variables(0).ValuesAsNumpy(),
+        "precipitation_sum": daily.Variables(1).ValuesAsNumpy(),
+        "wind_speed_10m_max": daily.Variables(2).ValuesAsNumpy(),
+    })
+
+
+
+def fetch_weather_data_batches(
+    openmeteo: openmeteo_requests.Client,
+    locations: pd.DataFrame,
+    batch_size: int = 10,
+) -> pd.DataFrame:
+    """Fetch weather data from open-meteo API in batches."""
+
     all_frames = []
     total_batches = (len(locations) + batch_size - 1) // batch_size
 
@@ -55,64 +103,31 @@ def fetch_weather_batches(openmeteo, locations, batch_size=10):
         }
         
         # get responses from openmeteo
-        # since open-meteo limits the amount of data you can get per minute this try and except block is needed
-        tries = 0
-        while True:
-            try:
-                tries += 1
-                responses = openmeteo.weather_api(URL, params=params)
-                print(f"request succeeded on try {tries}")
-                break
-            except Exception as e:
-                print(f"request failed on try {tries}, waiting 60 seconds: {e}")
-                time.sleep(60)
-
+        responses = request_with_retry(openmeteo, params)
 
         for response in responses:
-            daily = response.Daily()
-
-            dates = pd.date_range(
-                start=pd.to_datetime(daily.Time(), unit="s", utc=True),
-                periods=len(daily.Variables(0).ValuesAsNumpy()),
-                freq="D",
-            )
-
-            df = pd.DataFrame({
-                "longitude": response.Longitude(),
-                "latitude": response.Latitude(),
-                "year": dates.year,
-                "month": dates.month,
-                "day": dates.day,
-                "temperature_2m_mean": daily.Variables(0).ValuesAsNumpy(),
-                "temperature_2m_max": daily.Variables(1).ValuesAsNumpy(),
-                "temperature_2m_min": daily.Variables(2).ValuesAsNumpy(),
-                "precipitation_sum": daily.Variables(3).ValuesAsNumpy(),
-                "wind_speed_10m_max": daily.Variables(4).ValuesAsNumpy(),
-            })
-
-            all_frames.append(df)
-
+            all_frames.append(response_to_dataframe(response))
 
     return pd.concat(all_frames, ignore_index=True)
 
 
-# MAIN
+# MAIN SCRIPT
 
 def main():
 
     # setup api client
     openmeteo = setup_api_client()
 
-    # load file with locations
+    # load file with locations data containing latitudes and longitudes
     locations = pd.read_csv(LOCATIONS_FILE).head(100) # TODO remove .head(100) when production
 
-    # get data from api for each location
-    weather_df = fetch_weather_batches(openmeteo, locations, batch_size=10)
+    # get wether data from open-meteo api for each location in batches.
+    weather_df = fetch_weather_data_batches(openmeteo, locations, batch_size=10)
 
     # add metadata
-    weather_df["load_timestamp"] = datetime.now(timezone.utc).replace(microsecond=0)
-    weather_df["source"] = "https://open-meteo.com"
-    weather_df["run_id"] = str(uuid.uuid4())
+    weather_df["load_timestamp"]    = datetime.now(timezone.utc).replace(microsecond=0)
+    weather_df["source"]            = "https://open-meteo.com"
+    weather_df["run_id"]            = str(uuid.uuid4())
 
     #weather_df.to_parquet(OUTPUT_DIR / "weather_daily", index=False, partition_cols=["year"])
     weather_df.to_csv(OUTPUT_DIR / "weather_daily.csv", index=False) # TODO replace with parquet when finished
